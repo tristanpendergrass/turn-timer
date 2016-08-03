0193f05a89b1d56f3ef97a6b541a7ae460e3294b
Tristan <tristanpendergrass@gmail.com>
2016-08-03 09:39:55 -0700

    Make countdown work with start/stop button

--- js/millisecondsLabel.filter.js
+++ js/millisecondsLabel.filter.js
@@ -1,6 +1,11 @@
 angular.module('turnTimer')
 .filter('millisecondsLabel', function() {
   return function(input) {
-    return '3:00';
+    var date = new Date(1970, 0, 1);
+    if (input > 0) {
+      date.setMilliseconds(input);
+    }
+
+    return date.getMinutes() + 'm ' + date.getSeconds() % 60 + 's ' + date.getMilliseconds() % 1000 + 'ms';
   };
 });
--- js/player.directive.js
+++ js/player.directive.js
@@ -1,9 +1,30 @@
-angular.module('turnTimer').directive('player', function() {
+angular.module('turnTimer').directive('player', function($interval) {
   return {
     restrict: 'E',
     templateUrl: 'templates/player.html',
     link: function($scope) {
-      $scope.timeLeft = '1000';
+      $scope.timeLeft = 10000;
+
+      $scope.toggle = function() {
+        if ($scope.inProgress) {
+          $interval.cancel($scope.countdown);
+          $scope.inProgress = false;
+        } else {
+          const now = Date.parse(new Date());
+          const mark = new Date(now + $scope.timeLeft);
+
+          $scope.countdown = $interval(function() {
+            $scope.timeLeft = mark.getTime() - new Date().getTime();
+
+            if ($scope.timeLeft < 1) {
+              $interval.cancel($scope.countdown);
+              $scope.inProgress = false;
+            }
+          }, 1);
+
+          $scope.inProgress = true;
+        }
+      };
     },
     scope: {
       player: '='
